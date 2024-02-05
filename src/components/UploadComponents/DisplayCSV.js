63924ecf27641021caf206f117f0631703d143c5
Tanmayi Manku <tanmayitanum@gmail.com>
2024-02-05 13:59:30 +0530

    upload file and display data, implement crud for tags

--- src/components/UploadComponents/DisplayCSV.js
+++ src/components/UploadComponents/DisplayCSV.js
@@ -1,1 +1,94 @@
-// Upload page, upload file section.
+import React, { useState } from "react";
+import Select from "react-select";
+
+const DisplayCSV = ({ data }) => {
+  console.log('data from the displayCSV component', data);
+
+  const columnHeadings = Object.keys(data[0] || {});
+  const [selectedTagsPerRow, setSelectedTagsPerRow] = useState({});
+
+  const handleTagSelect = (rowIndex, tag) => {
+    console.log(`rowIndex: ${rowIndex}`);
+    console.log(`tag:${tag.value}`)
+    console.log(`selectedTagsPerRow[rowIndex]: ${selectedTagsPerRow[rowIndex]}`)
+    const tagsArray = selectedTagsPerRow[rowIndex] || [];
+    tagsArray.push(tag.value)
+    setSelectedTagsPerRow({
+        ...selectedTagsPerRow,
+        [rowIndex]: tagsArray,
+    })
+    console.log('selectedTags: ', tagsArray);
+  }
+
+  console.log('selectedTagsPerRow: ', selectedTagsPerRow);
+
+  const handleRemoveTag = (rowIndex, tagIndex) => {
+    console.log('outer_index: ', rowIndex);
+    console.log('e.target: ', tagIndex);
+    console.log('deleting tag is: ', tagIndex.value);
+    const tagsArray = selectedTagsPerRow[rowIndex] || [];
+    tagsArray.splice(tagIndex, 1);
+    setSelectedTagsPerRow({
+      ...selectedTagsPerRow,
+      [rowIndex]: tagsArray,
+    });
+  }
+
+  return (
+    <div className="csv-file-data-display-inner">
+      <h3 className="file-data-section-title">Uploads</h3>
+      <div className="data-table-outer">
+      <table className="file-data-section-table">
+        <thead>
+          <tr className="table-headings-row">
+            {columnHeadings.map((heading) => (
+              <th key={heading} className={`th-${heading.replace(/\s+/g, '-')}`}>
+                {heading.toLowerCase() === "id" ? (
+                    "SI No."
+                  ) : (heading)}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, outer_index) => (
+            <tr key={outer_index} className="table-data-rows" id={`row-${outer_index}`}>
+              {columnHeadings.map((heading, index) => (
+                <td key={heading} id={`cell-${outer_index}-${index}`} className={`td-${heading.replace(/\s+/g, '-')}`}>
+                  {heading.toLowerCase() === "links" ? (
+                    <a href={`https://${item[heading]}`} target="_blank" rel="noopener">
+                      {item[heading]}
+                    </a>
+                  ) : heading.toLowerCase() === "select tags" ? (
+                    <label htmlFor={`select-tags-${outer_index}`}>
+                      <Select
+                        className="areas-select-tag"
+                        classNamePrefix="areas-select-tag"
+                        id={`select-tags-${outer_index}`}
+                        options={item[heading].split(',').map((tag, i) => ({ value: tag, label: tag }))}
+                        onChange={(selectedOption) => handleTagSelect(outer_index, selectedOption)}
+                        placeholder="Select Tags"
+                      />
+                    </label>
+                  ) : (
+                    heading.toLowerCase() === "selected tags" ? (
+                      selectedTagsPerRow[outer_index] ? (
+                        selectedTagsPerRow[outer_index].map((tag, i) => <span className="each-selected-tag" key={i}>{tag}<button className="selected-tag-delete-button" type="button" onClick={(e) => handleRemoveTag(outer_index, i)}>&#x00D7;</button></span>
+                        )
+                      ) : null
+                    ) : (
+                      item[heading]
+                    )
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayCSV;
--- src/components/UploadComponents/UploadCSV.js
+++ src/components/UploadComponents/UploadCSV.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import Papa from 'papaparse';
-import UploadFile from '../../assets/upload-icon.png';
-import ExcelIcon from '../../assets/excel-icon.png';
+import React, { useState } from "react";
+import Papa from "papaparse";
+import UploadFile from "../../assets/upload-icon.png";
+import ExcelIcon from "../../assets/excel-icon.png";
+import DisplayCSV from "./DisplayCSV";
+import LoadingIcon from '../../assets/loading-icon.png';
 
-const allowedExtensions = ['csv', 'xlsx', 'xls'];
+const allowedExtensions = ["csv", "xlsx", "xls"];
 
 const UploadCSV = () => {
   // store selected file name
@@ -11,76 +13,134 @@ const UploadCSV = () => {
 
   // store file parsed data
   const [fileData, setFileData] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
+  const [uploadData, setUploadData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFileChange = (e) => {
-    setError('');
+    setError("");
 
     if (e.target.files.length) {
       const file = e.target.files[0];
 
-      const fileExtension = file?.type.split('/')[1];
+      const fileExtension = file?.type.split("/")[1];
       if (!allowedExtensions.includes(fileExtension)) {
         setError("Doesn't support the selected file.");
         setTimeout(() => {
-          setError('');
+          setError("");
         }, 3000);
         return;
       }
+
       setSelectedFile(file);
     }
   };
 
   const handleRemoveFile = () => {
     setSelectedFile(null);
+    setFileData([]);
+    setUploadData(false);
+  };
+
+  const handleParseDataDisplay = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if (selectedFile) {
+      await parseFileData(selectedFile);
+    } else {
+      setError("Please select a file.");
+      setIsLoading(false);
+    }
+  };
+
+  const parseFileData = (file) => {
+    return new Promise((resolve) => {
+      Papa.parse(file, {
+        complete: (result) => {
+          console.log("Parsed CSV data:", result.data);
+          setFileData(result.data);
+          setUploadData(true);
+          setIsLoading(false);
+          resolve();
+        },
+        header: true,
+      });
+    });
   };
 
   return (
-    <div className="upload-file-div-outer">
-      <div className="upload-drag-div">
-        <img src={ExcelIcon} alt="Excel-icon" className="upload-excel-icon" />
-        {selectedFile ? (
-          <>
-            <div className="selected-file-div">
-              <span className="selected-file-name">{selectedFile.name}</span>
-              <button
-                type="button"
-                id="remove-selected-csv-file"
-                onClick={handleRemoveFile}
-              >
-                Remove
-              </button>
+    <>
+      <div className="upload-file-div-outer flex-column">
+        <div className="upload-drag-div flex-column">
+          <img src={ExcelIcon} alt="Excel-icon" className="upload-excel-icon" />
+          {selectedFile ? (
+            <>
+              <div className="selected-file-div">
+                <span className="selected-file-name">{selectedFile.name}</span>
+                <button
+                  type="button"
+                  id="remove-selected-csv-file"
+                  onClick={handleRemoveFile}
+                >
+                  Remove
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="upload-drag-div-browse-text">
+                <span>Drop your excel sheet here or </span>
+                <label htmlFor="csv-file" className="csv-file-label">
+                  browse
+                  <input
+                    id="csv-file"
+                    name="csv-file"
+                    type="File"
+                    placeholder="browse"
+                    style={{ display: "none" }}
+                    onChange={handleFileChange}
+                  />
+                </label>
+              </div>
+            </>
+          )}
+          {error && (
+            <div className="error-message">
+              <span>{error}</span>
             </div>
-          </>
-        ) : (
-          <>
-            <div className="upload-drag-div-browse-text">
-              <span>Drop your excel sheet here or </span>
-              <label htmlFor="csv-file" className="csv-file-label">
-                browse
-                <input
-                  id="csv-file"
-                  name="csv-file"
-                  type="File"
-                  placeholder="browse"
-                  style={{ display: 'none' }}
-                  onChange={handleFileChange}
-                />
-              </label>
-            </div>
-          </>
-        )}
-        {error && (
-          <div className="error-message">
-            <span>{error}</span>
-          </div>
-        )}
+          )}
+        </div>
+        <button
+          type="button"
+          className={`upload-button upload-button-${uploadData ? 'disabled' : 'normal'}`}
+          onClick={handleParseDataDisplay}
+          disabled={uploadData}
+        >
+          {isLoading ? (
+            <img
+              src={LoadingIcon}
+              alt="loading-icon"
+              className="loading-icon"
+            />
+          ) : (
+            <>
+              <img
+                src={UploadFile}
+                alt="upload-icon"
+                className="upload-file-icon"
+              />
+              <span className="button-text">Upload</span>
+            </>
+          )}
+        </button>
       </div>
-      <button type="button" className="upload-button">
-        <img src={UploadFile} alt="upload-icon" className="upload-file-icon" />
-        <span className="button-text">Upload</span>
-      </button>
-    </div>
+      {uploadData ? (
+        <div className="upload-data-display-outer">
+          <DisplayCSV data={fileData} />
+        </div>
+      ) : ''}
+    </>
   );
 };
 
